3407fd13075656609bebf466bcd08528a73754f6
ElvisChenML <wangwei0@gmail.com>
2024-07-28 21:41:19 +0800

    NewChatVoice 1.1
    * 新增 外部调用接口。
    
      * 外部调用将使用相同的插件配置文件，但无视voice_switch状态。
    
    * 优化 配置文件逻辑。
    
      * 配置将分为 通用配置 “conifg.yaml”，以及会话配置 “config_会话.yaml”
      * 会话配置 优先级高于 通用配置

--- README.md
+++ README.md
@@ -8,15 +8,60 @@
 
 特点：速度快，低价，效果好，支持原神、星铁、日漫1000+个角色、鸣潮、红警、APEX等多种类型的角色音色
 
-当前版本：v1.0
-
 TODO：
 
 - [ ] 支持更多TTS平台，包括在线和本地的，如[GPT-SoVITS](https://github.com/RVC-Boss/GPT-SoVITS)
-- [ ] 支持用户单独配置token等鉴权密钥
 - [ ] 支持返回更长语音
-- [ ] 联动其他插件，如[Waifu](https://github.com/ElvisChenML/Waifu)
 - [ ] 可能的WebUI配置页面，实现支持在线页面切换
+
+## 版本记录
+
+### NewChatVoice 1.1
+
+* 新增 外部调用接口。
+
+  * 外部调用将使用相同的插件配置文件，但无视voice_switch状态。
+
+  * 接口函数：
+
+    ```python
+    async def ncv_tts(self, user_id: str, text: str) -> Voice:
+        """
+        供外部调用的文字转Voice的接口
+    
+        Args:
+            user_id (str): 会话ID
+            text (str): 要转换的文本
+    
+        Returns:
+            Voice: 生成的语音对象
+        """
+    ```
+
+  * 调用示例：
+
+    ```python
+    async def handle_voice_synthesis(self, launcher_id: int, text: str, ctx: EventContext):
+        try:
+            from plugins.NewChatVoice.main import VoicePlugin, VoiceSynthesisError
+        except ImportError as e:
+            self.ap.logger.error(f"Failed to import VoicePlugin: {e}")
+            return False
+    
+        ncv = VoicePlugin(self.host)
+        try:
+            voice = await ncv.ncv_tts(launcher_id, text)
+            await ctx.event.query.adapter.reply_message(ctx.event.query.message_event, MessageChain([voice]), False)
+            return True
+        except VoiceSynthesisError as e:
+            self.ap.logger.error(f"{e}")
+            return False
+    ```
+
+* 优化 配置文件逻辑。
+
+  * 配置将分为 通用配置 “conifg.yaml”，以及会话配置 “config_&#91;会话&#93;.yaml”
+  * 会话配置 优先级高于 通用配置
 
 ## NewChatVoice插件使用（重要）
 
@@ -42,16 +87,19 @@ TODO：
 打开NewChatVoice的config文件夹下的`config.yaml`，内容如下所示
 
 ```yaml
-# 默认角色，可在对话中指定角色，不指定则为每个用户使用默认角色
-character: "派蒙"
+# 语音角色id，可在对话中指定角色，不指定则为使用默认角色（派蒙 430）
+character_id: 430
 # 是否为每个用户默认开启语音功能，默认为False，即不开启，True为默认开启
 voice_switch: False
-
+# 语音传输方式：path/base64，如果遇到消息平台访问权限问题，可尝试切换
+voice_type: path
+# 语音缓存路径，如果遇到消息平台访问权限问题，可以尝试修改至消息平台有权限访问的目录
+temp_dir_path: plugins/NewChatVoice/audio_temp
 # token，这个是必须的，不然无法使用，获取方式请看文档
-token: "填入购买后显示的网站，不能删首尾的双引号"
+token: 填入购买后显示的网站
 ```
 
-重点关注第三个，token，在汉字提示处填入刚刚复制网址，保存即可
+重点关注：token，在汉字提示处填入刚刚复制网址，保存即可
 
 ## 指令（重要）
 
--- pkg/user_prefer.py
+++ pkg/user_prefer.py
@@ -1,71 +1,63 @@
 import json
-import os
-
-# 文件内容示例
-# {
-#   "123456": {
-#     "switch": true,
-#     "provider": "haitunAI",
-#     "detail": {
-#       "voice_id": "430",
-#       "voice_name": "派蒙"
-#     }
-#   }
-# }
-
-# 获取当前脚本所在的目录
-current_dir = os.path.dirname(os.path.abspath(__file__))
-
-# 构建绝对路径
-file_path = os.path.join(current_dir, '..', 'data', 'preference.json')
-
-# 如果目录和文件不存在则创建
-if not os.path.exists(os.path.join(current_dir, '..')):
-    os.mkdir(os.path.join(current_dir, '..'))
-if not os.path.exists(file_path):
-    with open(file_path, 'w', encoding='utf-8') as file:
-        file.write('{}')
+from plugins.NewChatVoice.pkg.config import ConfigManager
 
 
-def read_json(file_path):
-    with open(file_path, 'r', encoding='utf-8') as file:
-        return json.load(file)
+class UserPreference:
+    def __init__(self, launcher_id: str = ""):
+        self.launcher_id = launcher_id
+        self.config = ConfigManager("plugins/NewChatVoice/config/config", "plugins/NewChatVoice/templates/config", launcher_id=self.launcher_id)
+        self.character_path = "plugins/NewChatVoice/data/character.json"
+        self.temp_dir_path = "plugins/NewChatVoice/audio_temp"
+        self.voice_switch = True
+        self.character_id = 430
+        self.character_dict = {}
+        self.voice_type = "path"
+        self.token = ""
 
+    async def load_config(self) -> None:
+        """
+        加载配置文件，并更新实例变量
+        """
+        await self.config.load_config(completion=True)
+        self.voice_switch = self.config.data.get("voice_switch", True)
+        self.character_id = self.config.data.get("character_id", 430)
+        self.voice_type = self.config.data.get("voice_type", "path")
+        self.temp_dir_path = self.config.data.get("temp_dir_path", "plugins/NewChatVoice/audio_temp")
+        self.token = self.config.data.get("token","")
+        self.character_dict = await self._load_character_dict()
 
-def write_json(file_path, data):
-    with open(file_path, 'w', encoding='utf-8') as file:
-        json.dump(data, file, ensure_ascii=False, indent=4)
+    async def change_preference(self, content: dict) -> None:
+        """
+        更新用户偏好设置，并保存到配置文件
+        调用示例：change_preference({"switch": True})
+        """
+        preferences = ["voice_switch", "character_id", "voice_type"]
 
+        for preference in preferences:
+            if preference in content:
+                self.config.data[preference] = content[preference]
+                await self.config.update_config(preference, content[preference])
 
-# 查询用户偏好
-def get_preference(user_id):
-    # 检测内容，如果为空，则写入{}
-    if os.path.getsize(file_path) == 0:
-        write_json(file_path, {})
-    # 如果user_id为数字，则转为字符串
-    if isinstance(user_id, int):
-        user_id = str(user_id)
-    data = read_json(file_path)
-    if user_id not in data:
-        # print(f"用户 {user_id} 不存在。")
-        return {}
-    return data[user_id]
+        # 重新加载配置
+        await self.load_config()
 
+    async def get_character_info(self, id: str) -> dict:
+        return self.character_dict.get(id)
 
-# 修改用户偏好,包括增改
-def change_preference(user_id, content):
-    # print(f"-------修改用户{user_id}的偏好为{content}")
-    data = read_json(file_path)
-    # content有三种形式，1是仅控制开关，2是只修改详细，3是控制开关和修改详细
-    # 如果content中有switch和detail，则同时修改
-    if "switch" in content and "detail" in content:
-        data[user_id] = content
-    # 如果content中只有switch，则直接修改开关
-    if "switch" in content:
-        data[user_id]["switch"] = content["switch"]
-    # 如果content中只有detail，则直接修改详细
-    if "detail" in content:
-        data[user_id]["detail"] = content["detail"]
+    async def _load_character_dict(self) -> dict:
+        # 若已加载过则不重新加载
+        if self.character_dict:
+            return self.character_dict
+        try:
+            with open(self.character_path, "r", encoding="UTF-8") as file:
+                character_list = json.load(file)
+                return {str(ch["id"]): ch for ch in character_list}
+        except Exception as e:
+            print(f"Error loading characters: {e}")
+            return {}
 
-    write_json(file_path, data)
-    return content
+    def get_character_by_id(self) -> None:
+        if str(self.character_id) in self.character_dict:
+            return self.character_dict[str(self.character_id)]["voice_name"]
+        else:
+            return "未知角色"
