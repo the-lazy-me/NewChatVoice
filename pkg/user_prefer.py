a12b6c21e92ec557d6f58c4b71506036721d6f06
ElvisChenML <wangwei0@gmail.com>
2024-08-01 13:43:07 +0800

    NewChatVoice 1.2
    * 修改 配置文件位置，为了避免升级时被删除，过程文件及配置文件目录移至插件目录外：“QChatGPT\data\plugins\NewChatVoice\”。

--- pkg/user_prefer.py
+++ pkg/user_prefer.py
@@ -5,9 +5,9 @@ from plugins.NewChatVoice.pkg.config import ConfigManager
 class UserPreference:
     def __init__(self, launcher_id: str = ""):
         self.launcher_id = launcher_id
-        self.config = ConfigManager("plugins/NewChatVoice/config/config", "plugins/NewChatVoice/templates/config", launcher_id=self.launcher_id)
-        self.character_path = "plugins/NewChatVoice/data/character.json"
-        self.temp_dir_path = "plugins/NewChatVoice/audio_temp"
+        self.config = ConfigManager("data/plugins/NewChatVoice/config/config", "plugins/NewChatVoice/templates/config", launcher_id=self.launcher_id)
+        self.character_path = "data/plugins/NewChatVoice/data/character.json"
+        self.temp_dir_path = "data/plugins/NewChatVoice/audio_temp"
         self.voice_switch = True
         self.character_id = 430
         self.character_dict = {}
@@ -22,7 +22,7 @@ class UserPreference:
         self.voice_switch = self.config.data.get("voice_switch", True)
         self.character_id = self.config.data.get("character_id", 430)
         self.voice_type = self.config.data.get("voice_type", "path")
-        self.temp_dir_path = self.config.data.get("temp_dir_path", "plugins/NewChatVoice/audio_temp")
+        self.temp_dir_path = self.config.data.get("temp_dir_path", "data/plugins/NewChatVoice/audio_temp")
         self.token = self.config.data.get("token","")
         self.character_dict = await self._load_character_dict()
 
